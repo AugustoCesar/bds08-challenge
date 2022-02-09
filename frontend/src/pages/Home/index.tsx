36e8c1291a8e9c12973245378de93ddc4f2ebfe5
Augusto Nunes <augustocesar.udi@gmail.com>
2022-02-09 07:42:49 -0300

    Home page styles

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -3,13 +3,21 @@ import { Link } from 'react-router-dom';
 import './styles.css';
 
 const Home = () => {
-    return (
-        <div className="home-container">
-            <Link to="/gitsearch">
-                <button className="btn btn-primary btn-lg start-button">Começar</button>
-            </Link>
-        </div>
-    );
-}
+  return (
+    <div className="home-container">
+      <div>
+        <h1>Desafio Github API</h1>
+        <p>Bootcamp Spring React - DevSuperior</p>
+      </div>
+      <div>
+        <Link to="/gitsearch">
+          <button className="btn btn-primary btn-lg start-button">
+            Começar
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
