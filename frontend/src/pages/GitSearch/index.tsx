4137f9a1c84fb2bca8beecd44f335fff78e115f5
Augusto Nunes <augustocesar.udi@gmail.com>
2022-02-09 06:33:57 -0300

    Project created and structured

---
+++ frontend/src/pages/GitSearch/index.tsx
@@ -0,0 +1,74 @@
+import './styles.css';
+
+import ResultCard from 'components/ResultCard';
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+
+type FormData = {
+  cep: string;
+};
+
+type Address = {
+  logradouro: string;
+  localidade: string;
+};
+
+const GitSearch = () => {
+  const [address, setAddress] = useState<Address>();
+
+  const [formData, setFormData] = useState<FormData>({
+    cep: '',
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    axios
+      .get(`https://viacep.com.br/ws/${formData.cep}/json/`)
+      .then((response) => {
+        setAddress(response.data);
+      })
+      .catch((error) => {
+        setAddress(undefined);
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="git-search-container">
+      <h1 className="text-primary">Encontre um perfil Github</h1>
+      <div className="container search-container">
+        <form onSubmit={handleSubmit}>
+          <div className="form-container">
+            <input
+              type="text"
+              name="usuario"
+              className="search-input"
+              placeholder="Usuário Github"
+              value={formData.cep}
+              onChange={handleChange}
+            />
+            <button type="submit" className="btn btn-primary search-button">
+              Encontrar
+            </button>
+          </div>
+        </form>
+
+        {address &&
+          <>
+            <ResultCard title="Result" description={address.logradouro} />
+          </>
+        }
+      </div>
+    </div>
+  );
+};
+
+export default GitSearch;
